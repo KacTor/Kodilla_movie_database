848bdc29859f7b4c24e73772d60e968da9514e9d
Kacper Torba <ka.toorba@gmail.com>
2023-01-30 15:42:08 +0100

    Create movie_series_database file

---
+++ Movie_series_database.py
@@ -0,0 +1,31 @@
+class Movie:
+
+    def __init__(self, title, releaseDate, genre, numberOfPlays) -> None:
+        self.title = title
+        self.releaseDate = releaseDate
+        self.genre = genre
+        self.numberOfPlays = numberOfPlays
+
+    def play(self):
+        self.numberOfPlays += 1
+
+    def __str__(self) -> str:
+        return f'{self.title} ({self.releaseDate})'
+
+
+class TvSeries(Movie):
+
+    def __init__(self, numberOfSeason, numberOfEpisode, *args) -> None:
+        super().__init__(*args)
+        self.numberOfSeason = numberOfSeason
+        self.numberOfEpisode = numberOfEpisode
+
+    def __str__(self) -> str:
+        return f'{self.title} S{self.numberOfSeason:02d}E{self.numberOfEpisode:02d}.'
+
+
+ex = TvSeries(2, 3, '3', 's', 'd', 4)
+
+ex.play()
+
+print(ex)
